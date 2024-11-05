7ef0ce143170f651a35f5c4617f609973258b95d
Tahsin <alahitahsin@gmail.com>
2024-11-05 17:39:53 +0600

    made homepage header responsive

--- src/component/HomeHeader.jsx
+++ src/component/HomeHeader.jsx
@@ -2,10 +2,10 @@ import bannerImg from "../assets/banner.jpg";
 
 function HomeHeader() {
   return (
-    <header className="h-[calc(100vh-75px)] p-2 pt-0 border border-gray-700/30 border-t-0 rounded-b-2xl mb-96">
-      <div className="bg-purple-700 h-full w-full relative rounded-b-2xl">
+    <header className="h-[calc(100vh-75px)] p-2 pt-0 border border-gray-700/30 border-t-0 rounded-b-2xl lg:mb-96 mb-56 ">
+      <div className="bg-purple-700 h-full w-full  rounded-b-2xl">
         <div className="text-center w-2/3 mx-auto pt-12">
-          <h1 className="text-4xl font-bold text-white mb-5">
+          <h1 className="lg:text-4xl text-3xl font-bold text-white mb-5">
             Upgrade Your Tech Accessorize with Gadget Heaven Accessories
           </h1>
           <p className="text-gray-200 mb-5">
@@ -13,11 +13,11 @@ function HomeHeader() {
             next level. From smart devices to the coolest accessories, we have
             it all!
           </p>
-          <button className="px-6 py-2 bg-white text-xl font-semibold rounded-3xl text-purple-700 hover:bg-slate-200">
+          <button className="lg:px-6 py-2 px-4 bg-white text-xl font-semibold rounded-3xl text-purple-700 hover:bg-slate-200">
             Shop Now
           </button>
         </div>
-        <div className="absolute w-2/3 top-1/2 mt-10 left-1/2 -translate-x-1/2 p-4 border-2 border-white rounded-3xl backdrop-blur-md object-cover shadow-md">
+        <div className=" lg:w-2/3 w-full lg:mt-10 mt-14 mx-auto p-4 border-2 border-white rounded-3xl backdrop-blur-md object-cover shadow-md">
           <img
             src={bannerImg}
             alt="Banner image"
@@ -28,5 +28,4 @@ function HomeHeader() {
     </header>
   );
 }
-
 export default HomeHeader;
